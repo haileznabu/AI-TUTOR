30fa6086eed5238e1f6907731b5194234666be42
HAILE ZNABU <43222722+haileznabu@users.noreply.github.com>
2025-10-28 22:21:37 -0900

    Updated main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
